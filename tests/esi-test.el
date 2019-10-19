56f8e4f9bc4b7e3150d773aaaef0469240a06c73
Abhinav Tushar <lepisma@fastmail.com>
2019-10-19 16:05:58 +0530

    use io functions from esi-utils in test

--- tests/esi-test.el
+++ tests/esi-test.el
@@ -1,4 +1,5 @@
 (require 'esi-core)
+(require 'esi-utils)
 (require 'cl-extra)
 (require 'cl-lib)
 (require 'f)
@@ -7,14 +8,6 @@
 (describe "Version"
   (it "is correct"
     (expect (esi-core-version) :to-equal "0.0.2")))
-
-(defun matrix-from-file (filepath)
-  "Read a numpy.savetxt style matrix from given file."
-  (cl-map 'vector (lambda (line) (cl-map 'vector #'string-to-number (s-split " " line))) (s-split "\n" (s-trim (f-read-text filepath)))))
-
-(defun vector-from-file (filepath)
-  "Read a newline separated vector (also numpy.savetxt style) from given file."
-  (cl-map 'vector #'string-to-number (s-split "\n" (s-trim (f-read-text filepath)))))
 
 (defun fft-real (vector)
   (cl-map 'vector (lambda (elem) (aref elem 0)) vector))
@@ -33,47 +26,47 @@
     (let ((samples (esi-core-wav-to-samples (with-temp-buffer
                                               (insert-file-contents-literally "tests/resources/hello.wav")
                                               (buffer-string))))
-          (true-samples (vector-from-file "tests/resources/hello.samples")))
+          (true-samples (esi-utils-load-array "tests/resources/hello.samples")))
       (expect (vector-approx-equal true-samples samples) :to-be t))))
 
 (describe "FFT"
   (it "has correct real values"
-    (let* ((samples (vector-from-file "tests/resources/hello.samples"))
+    (let* ((samples (esi-utils-load-array "tests/resources/hello.samples"))
            (fft (esi-core--rfft samples))
-           (true-real-values (vector-from-file "tests/resources/hello.fft.real")))
+           (true-real-values (esi-utils-load-array "tests/resources/hello.fft.real")))
       (expect (vector-approx-equal true-real-values (fft-real fft)))))
   (it "has correct imaginary values"
-    (let* ((samples (vector-from-file "tests/resources/hello.samples"))
+    (let* ((samples (esi-utils-load-array "tests/resources/hello.samples"))
            (fft (esi-core--rfft samples))
-           (true-imag-values (vector-from-file "tests/resources/hello.fft.imag")))
+           (true-imag-values (esi-utils-load-array "tests/resources/hello.fft.imag")))
       (expect (vector-approx-equal true-imag-values (fft-imag fft))))))
 
 (describe "STFT"
   (it "has correct real values"
-    (let* ((samples (vector-from-file "tests/resources/hello.samples"))
+    (let* ((samples (esi-utils-load-array "tests/resources/hello.samples"))
            (stft (esi-core--stft samples 2048 512))
-           (true-real-values (matrix-from-file "tests/resources/hello.stft.real")))
+           (true-real-values (esi-utils-load-array "tests/resources/hello.stft.real")))
       (expect (matrix-approx-equal true-real-values (stft-real stft)))))
   (it "has correct imaginary values"
-    (let* ((samples (vector-from-file "tests/resources/hello.samples"))
+    (let* ((samples (esi-utils-load-array "tests/resources/hello.samples"))
            (stft (esi-core--stft samples 2048 512))
-           (true-imag-values (matrix-from-file "tests/resources/hello.stft.imag")))
+           (true-imag-values (esi-utils-load-array "tests/resources/hello.stft.imag")))
       (expect (matrix-approx-equal true-imag-values (stft-imag stft))))))
 
 (describe "Spectrogram"
   (it "is correct"
-    (let ((samples (vector-from-file "tests/resources/hello.samples"))
-          (true-spectrogram (matrix-from-file "tests/resources/hello.spectrogram")))
+    (let ((samples (esi-utils-load-array "tests/resources/hello.samples"))
+          (true-spectrogram (esi-utils-load-array "tests/resources/hello.spectrogram")))
       (expect (matrix-approx-equal true-spectrogram (esi-core--spectrogram samples 2048 512 2))))))
 
 (describe "Mel filterbank"
   (it "is correct"
-    (let ((true-melfb (matrix-from-file "tests/resources/mel.fb"))
+    (let ((true-melfb (esi-utils-load-array "tests/resources/mel.fb"))
           (melfb (esi-core--mel-filter 8000 2048 12)))
       (expect (matrix-approx-equal melfb true-melfb)))))
 
 (describe "Mel spectrogram"
   (it "is correct"
-    (let ((samples (vector-from-file "tests/resources/hello.samples"))
-          (true-spectrogram (matrix-from-file "tests/resources/hello.mel.spectrogram")))
+    (let ((samples (esi-utils-load-array "tests/resources/hello.samples"))
+          (true-spectrogram (esi-utils-load-array "tests/resources/hello.mel.spectrogram")))
       (expect (matrix-approx-equal true-spectrogram (esi-core--mel-spectrogram samples 8000 2048 512 40))))))
