bcd4cbe1c5e73b590d2a12ae71147be5302157b6
Abhinav Tushar <lepisma@fastmail.com>
2019-10-26 11:22:12 +0530

    code for background recording

--- src/esi-io.h
+++ src/esi-io.h
@@ -1,7 +1,11 @@
 #pragma once
 
-#include <stdint.h>
 #include "sndfile.h"
+#include <math.h>
+#include <soundio/soundio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <unistd.h>
 
 typedef struct {
   sf_count_t offset, length;
@@ -87,4 +91,184 @@ double* samples_from_buffer(char* buffer, size_t buffer_size, sf_count_t* n_samp
   sf_close(sfile);
   free(frames);
   return samples;
+}
+
+
+static int min_int(int a, int b) {
+    return (a < b) ? a : b;
+}
+
+
+// Custom ring buffer. I might be wiser in using sio's implementation but there
+// I need to keep read pointer n-steps ahead and stuff to make it behave how I
+// want it to.
+struct buffer {
+  char* array;
+  size_t capacity;
+  size_t margin;
+  size_t write_offset;
+  size_t read_offset;
+};
+
+size_t s_max(size_t a, size_t b) {
+  return (b > a) ? b : a;
+}
+
+struct buffer* buffer_init(size_t capacity) {
+  struct buffer* buf = malloc(sizeof(struct buffer));
+
+  // TODO: Make this smaller and adaptive. At present this covers one second of
+  //       float-32 16k audio.
+  size_t margin = s_max(capacity, 32 * 16000);
+
+  buf->capacity = capacity;
+  buf->margin = margin;
+  buf->array = calloc(capacity + margin, sizeof(char));
+
+  buf->write_offset = 0;
+  buf->read_offset = buf->write_offset + buf->margin;
+
+  return buf;
+}
+
+void buffer_destroy(struct buffer* buf) {
+  free(buf->array);
+  free(buf);
+}
+
+// Write data_len bytes from data in buffer.
+bool buffer_write(struct buffer* buf, char* data, size_t data_len) {
+  size_t input_offset = 0;
+  if (data_len > buf->capacity) {
+    fprintf(stderr, "Data more than the capacity, taking the last chunks provided");
+    input_offset = (data_len - buf->capacity);
+    data_len = buf->capacity;
+  }
+
+  size_t buffer_length = buf->capacity + buf->margin;
+  for (size_t i = 0; i < data_len; i++) {
+    buf->array[(buf->write_offset + i) % buffer_length] = data[input_offset + i];
+  }
+
+  buf->write_offset = (buf->write_offset + data_len) % buffer_length;
+  buf->read_offset = (buf->write_offset + buf->margin) % buffer_length;
+  return true;
+}
+
+// Read upto a max of capacity from buffer and return the number of bytes read.
+size_t buffer_read(struct buffer* buf, char* output) {
+  size_t offset = buf->read_offset;
+  size_t buffer_length = buf->capacity + buf->margin;
+
+  for (size_t i = 0; i < buf->capacity; i++) {
+    output[i] = buf->array[(offset + i) % buffer_length];
+  }
+  return buf->capacity;
+}
+
+struct RecordContext {
+  struct buffer *buf;
+};
+
+static volatile bool keep_recording_flag = true;
+static volatile bool save_recording_flag = false;
+
+static void recording_read_callback(struct SoundIoInStream *instream, int frame_count_min, int frame_count_max) {
+  struct RecordContext *rc = instream->userdata;
+  struct SoundIoChannelArea *areas;
+  int err;
+
+  size_t free_count = rc->buf->capacity / instream->bytes_per_frame;
+
+  int write_frames = min_int(free_count, frame_count_max);
+  int frames_left = write_frames;
+
+  while (true) {
+    int frame_count = frames_left;
+
+    if ((err = soundio_instream_begin_read(instream, &areas, &frame_count))) {
+      fprintf(stderr, "begin read error: %s", soundio_strerror(err));
+      exit(1);
+    }
+
+    if (!frame_count)
+      break;
+
+    if (!areas) {
+      fprintf(stderr, "are error thing\n");
+    } else {
+      for (int frame = 0; frame < frame_count; frame += 1) {
+        for (int ch = 0; ch < instream->layout.channel_count; ch += 1) {
+          buffer_write(rc->buf, areas[ch].ptr, instream->bytes_per_sample);
+          areas[ch].ptr += areas[ch].step;
+        }
+      }
+    }
+
+    if ((err = soundio_instream_end_read(instream))) {
+      fprintf(stderr, "end read error: %s", soundio_strerror(err));
+      exit(1);
+    }
+
+    frames_left -= frame_count;
+    if (frames_left <= 0)
+      break;
+  }
+}
+
+static void recording_overflow_callback(struct SoundIoInStream *instream) {
+  static int count = 0;
+  fprintf(stderr, "overflow %d\n", ++count);
+}
+
+// Start a global recording thing.
+bool start_recording(size_t sample_rate, size_t buffer_duration_seconds, char* output_buffer) {
+  enum SoundIoBackend backend = SoundIoBackendPulseAudio;
+  struct RecordContext rc;
+  struct SoundIo *soundio = soundio_create();
+
+  soundio_connect_backend(soundio, backend);
+  soundio_flush_events(soundio);
+
+  struct SoundIoDevice *selected_device = NULL;
+
+  int device_index = soundio_default_input_device_index(soundio);
+  selected_device = soundio_get_input_device(soundio, device_index);
+
+  soundio_device_sort_channel_layouts(selected_device);
+
+  enum SoundIoFormat fmt = SoundIoFormatS16LE;
+
+  struct SoundIoInStream *instream = soundio_instream_create(selected_device);
+  instream->format = fmt;
+  instream->sample_rate = sample_rate;
+  instream->read_callback = recording_read_callback;
+  instream->overflow_callback = recording_overflow_callback;
+  instream->userdata = &rc;
+
+  soundio_instream_open(instream);
+
+  size_t buffer_capacity = buffer_duration_seconds * instream->sample_rate * instream->bytes_per_frame;
+  rc.buf = buffer_init(buffer_capacity);
+  soundio_instream_start(instream);
+
+  while (keep_recording_flag) {
+    soundio_flush_events(soundio);
+    sleep(1);
+
+    if (save_recording_flag) {
+      // TODO: Save to a global array here
+      printf("Saving recording");
+      save_recording_flag = false;
+    }
+  }
+
+  printf("Stopping recording");
+
+  soundio_instream_destroy(instream);
+  soundio_device_unref(selected_device);
+  soundio_destroy(soundio);
+  buffer_destroy(rc.buf);
+
+  return true;
 }
