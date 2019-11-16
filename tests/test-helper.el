5c3621f2d771c27632869688b98054a21ac5d308
Abhinav Tushar <abhinav@lepisma.xyz>
2019-11-16 12:36:45 +0530

    make test work on v26

--- tests/test-helper.el
+++ tests/test-helper.el
@@ -1,5 +1,6 @@
 (require 'cl-lib)
 (require 'dash)
+(require 'json)
 
 (defun approx-equal (x y &optional rtol atol)
   (<= (abs (- x y)) (+ (* (or rtol 1e-5) (abs y)) (or atol 1e-8))))
@@ -18,9 +19,11 @@
   "Run ffprobe result for first stream on FILEPATH and return
 sexp output. ARGS is a list of format option strings."
   (let ((file-arg (shell-quote-argument filepath))
-        (format-args (mapconcat #'shell-quote-argument args " ")))
+        (format-args (mapconcat #'shell-quote-argument args " "))
+        (json-object-type 'alist)
+        (json-array-type 'list))
     (--> (format "ffprobe -v quiet -print_format json -show_streams %s %s" format-args file-arg)
        (shell-command-to-string it)
-       (json-parse-string it :object-type 'alist :array-type 'list)
+       (json-read-from-string it)
        (alist-get 'streams it)
        (car it))))
