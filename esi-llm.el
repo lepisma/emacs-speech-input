a7f5740976716c8a8f800da2e80b3b73d1cc19fc
Abhinav Tushar <abhinav@lepisma.xyz>
2023-03-08 12:03:32 +0530

    More robust error handling and better formatting

--- esi-llm.el
+++ esi-llm.el
@@ -32,40 +32,58 @@
 (defvar esi-llm--buffer nil
   "Buffer for tracking LLM interactions.")
 
+(defcustom esi-llm-buffer-name "*esi-llm*"
+  "Name for the LLM interaction buffer."
+  :type 'string)
+
 (defcustom esi-llm-api-key nil
   "API key for using the LLM. This is injected in the call to
 process script."
   :type 'string)
 
-(defun esi-lm--md-to-org (text)
+(defun esi-llm--md-to-org (text)
+  "Assuming the LLM returns output in markdown (code blocks usually
+are like this), convert text to org-mode for better preview."
   (with-temp-buffer
     (insert text)
     (shell-command-on-region (point-min) (point-max) "pandoc -f markdown -t org" t t)
     (buffer-string)))
 
 (defun esi-llm--filter (proc string)
+  "Filter function for writing the LLM process output."
   (when (buffer-live-p (process-buffer proc))
     (with-current-buffer (process-buffer proc)
-      (let ((moving (= (point) (process-mark proc))))
-        (save-excursion
-          (goto-char (process-mark proc))
-          (insert (esi-lm--md-to-org (string-trim string)))
-          (set-marker (process-mark proc) (point)))
-        (if moving (goto-char (process-mark proc)))))))
+      (goto-char (process-mark proc))
+      (insert (esi-llm--md-to-org (string-trim string)))
+      (set-marker (process-mark proc) (point)))))
+
+(defun esi-llm--sentinel (proc event)
+  "Sentinel function for better handling exceptions from LLM script."
+  (message (format "Process %s sent event '%s'" proc event)))
+
+(defun esi-llm-running-p ()
+  "Tell if the LLM system is running."
+  (and (get-buffer esi-llm-buffer-name)
+       esi-llm--process
+       (process-live-p esi-llm--process)))
 
 (defun esi-llm-initialize ()
   "Start the LLM process."
   (let ((process-environment (cl-copy-list process-environment)))
     (setenv "OPENAI_API_KEY" esi-llm-api-key)
-    (setq esi-llm--buffer (get-buffer-create "*esi-llm*"))
+    (setq esi-llm--buffer (get-buffer-create esi-llm-buffer-name))
     (save-excursion
-      (with-current-buffer esi-llm--buffer
-        (esi-llm-mode)))
+      (with-current-buffer esi-llm--buffer (esi-llm-mode)))
+    (when esi-llm--process
+      (when (process-live-p esi-llm--process)
+        (kill-process esi-llm--process))
+      (setq esi-llm--process nil))
     (setq esi-llm--process
           (make-process :name "esi-llm"
                         :buffer esi-llm--buffer
                         :command '("chatgpt.py")
-                        :filter #'esi-llm--filter))))
+                        :filter #'esi-llm--filter
+                        :sentinel #'esi-llm--sentinel))))
 
 (defun esi-llm-write (text)
   "Write `text' to the process `esi-llm--process'."
@@ -73,7 +91,7 @@ process script."
     (with-current-buffer (process-buffer esi-llm--process)
       (save-excursion
         (goto-char (point-max))
-        (org-insert-heading)
+        (insert "\n* ")
         (org-insert-time-stamp (current-time) t t)
         (insert "\n")
         (insert "#+begin_quote\n" text "\n#+end_quote\n\n")
