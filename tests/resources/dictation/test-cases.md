fa6cf04f817da769b8cc28472b2c5f423eab0ab9
Abhinav Tushar <abhinav@lepisma.xyz>
2024-05-10 22:52:08 +0530

    add dummy tests for esi-dictate correction

---
+++ tests/resources/dictation/test-cases.md
@@ -0,0 +1,28 @@
+<!-- Test cases for dictation correction system. Each case's input is just one   -->
+<!-- continuous paragraph, rest of the content till a line starting with 5 - is  -->
+<!-- expected output. Instructions are separated with || in case your system can -->
+<!-- use them -->
+
+I want to write something that's difficult to transcribe and then try correcting that. Write my name as abcd. ||No separate the letters with . please.
+
+I want to write something that's difficult to transcribe and then try correcting that. Write my name as a.b.c.d.
+
+-----
+
+hi neumo, what are you doing? ||it's n e m o
+
+hi nemo, what are you doing?
+
+-----
+
+abhinav to sha. ||no it's t u s h a r after abhinav
+
+abhinav tushar
+
+-----
+
+Abhinav to Tushar ||remove the to
+
+Abhinav Tushar
+
+-----
--- tests/test-helper.el
+++ tests/test-helper.el
@@ -1,6 +1,9 @@
+;;; -*- lexical-binding: t; -*-
+
 (require 'cl-lib)
 (require 'dash)
 (require 'json)
+(require 'seq)
 
 (defun approx-equal (x y &optional rtol atol)
   (<= (abs (- x y)) (+ (* (or rtol 1e-5) (abs y)) (or atol 1e-8))))
@@ -27,3 +30,42 @@ sexp output. ARGS is a list of format option strings."
        (json-read-from-string it)
        (alist-get 'streams it)
        (car it))))
+
+(defun markdown-remove-comments ()
+  "Remove markdown style comments from the current buffer."
+  (goto-char (point-min))
+  (while (re-search-forward "<!--" nil t)
+    (let ((start (match-beginning 0)))
+      (when (re-search-forward "-->" nil t)
+        (delete-region start (match-end 0))))))
+
+(defun dictate-break-input (text)
+  "Break input text in content and command pair, if the separator is
+present in `text'."
+  (let ((splits (mapcar #'string-trim (string-split text "||"))))
+    (if (= (length splits) 2)
+        (cons (car splits) (cadr splits))
+      splits)))
+
+(defun dictate-break-case (text)
+  "Break test cases in input output along with content and command
+pair if the separator is there in input."
+  (let ((splits (mapcar #'string-trim (string-split (string-trim text) "\n\n"))))
+    (cons (dictate-break-input (car splits)) (cdr splits))))
+
+(defun dictate-parse-cases (filepath)
+  "Parse test cases from a markdown file and return a list."
+  (with-temp-buffer
+    (insert-file-contents filepath)
+    (markdown-remove-comments)
+    (goto-char (point-min))
+    (let ((cases)
+          (cursor (point)))
+      (while (not (eobp))
+        (if (re-search-forward "^-----$" nil t)
+            (let ((case-text (buffer-substring-no-properties cursor (match-beginning 0)))
+                  (end-point (match-end 0)))
+              (push (dictate-break-case case-text) cases)
+              (setq cursor end-point))
+          (goto-char (point-max))))
+      (reverse cases))))
